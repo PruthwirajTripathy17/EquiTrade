42d5caa94dc75535f09f575b5e84c6603e5a9352
Pruthwiraj Tripathy <pruthwirajtripathy2@gmail.com>
2026-06-06 16:38:48 +0530

    Convert frontend from submodule to normal folder

---
+++ frontend/src/landing_page/Footer.jsx
@@ -0,0 +1,305 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: "rgb(250,250,250)" }}>
+      <div className="container border-top mt-5">
+        <div className="row mt-5">
+          <div className="col">
+            <img
+              src="/media/Images/EquiTrade_logo.png"
+              style={{ width: "40%" }}
+            />
+            <p>
+              &copy; 2010 - 2024, Not EquiTrade Broking Ltd. All rights
+              reserved.
+            </p>
+          </div>
+          <div className="col text-muted">
+            <p className="fw-bold">Company</p>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              About
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              Products
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              Pricing
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              Referral programme
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              Careers
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              EquiTrade.tech
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              Press & media
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              EquiTrade cares(CSR)
+            </a>
+          </div>
+          <div className="col text-muted">
+            <p className="fw-bold">Support</p>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              Contacts
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              Support portal
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              Z-Connect blog
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              List of charges
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              Downloads & resourses
+            </a>
+          </div>
+          <div className="col text-muted">
+            <p className="fw-bold">Account</p>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              OPen an account
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              Fund transfer
+            </a>
+            <a
+              href=""
+              style={{ textDecoration: "none", color: "black" }}
+              className="d-block mb-2"
+            >
+              60 day challenge
+            </a>
+          </div>
+        </div>
+        <div className="mt-5 text-muted" style={{ fontSize: "14px" }}>
+          <p>
+            EquiTrade Broking Ltd.: Member of NSE, BSE, MCX & MSEI – SEBI
+            Registration no.: INZ000031633 CDSL/NSDL: Depository services
+            through EquiTrade Broking Ltd. – SEBI Registration no.: IN-DP-431-2019
+            Registered Address: EquiTrade Broking Ltd., #153/154, 4th Cross,
+            Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase,
+            Bengaluru - 560078, Karnataka, India. For any complaints pertaining
+            to securities broking please write to complaints@EquiTrade.com, for DP
+            related to dp@EquiTrade.com. Please ensure you carefully read the Risk
+            Disclosure Document as prescribed by SEBI | ICF
+          </p>
+
+          <p>
+            Procedure to file a complaint on SEBI SCORES: Register on SCORES
+            portal. Mandatory details for filing complaints on SCORES: Name,
+            PAN, Address, Mobile Number, E-mail ID. Benefits: Effective
+            Communication, Speedy redressal of the grievances
+          </p>
+
+          <p>
+            Smart Online Dispute Resolution | Grievances Redressal Mechanism
+          </p>
+
+          <p>
+            Investments in securities market are subject to market risks; read
+            all the related documents carefully before investing.
+          </p>
+
+          <p>
+            Attention investors: 1) Stock brokers can accept securities as
+            margins from clients only by way of pledge in the depository system
+            w.e.f September 01, 2020. 2) Update your e-mail and phone number
+            with your stock broker / depository participant and receive OTP
+            directly from depository on your e-mail and/or mobile number to
+            create pledge. 3) Check your securities / MF / bonds in the
+            consolidated account statement issued by NSDL/CDSL every month.
+          </p>
+
+          <p>
+            India's largest broker based on networth as per NSE. NSE broker
+            factsheet
+          </p>
+
+          <p>
+            "Prevent unauthorised transactions in your account. Update your
+            mobile numbers/email IDs with your stock brokers/depository
+            participants. Receive information of your transactions directly from
+            Exchange/Depositories on your mobile/email at the end of the day.
+            Issued in the interest of investors. KYC is one time exercise while
+            dealing in securities markets - once KYC is done through a SEBI
+            registered intermediary (broker, DP, Mutual Fund etc.), you need not
+            undergo the same process again when you approach another
+            intermediary." Dear Investor, if you are subscribing to an IPO,
+            there is no need to issue a cheque. Please write the Bank account
+            number and sign the IPO application form to authorize your bank to
+            make payment in case of allotment. In case of non allotment the
+            funds will remain in your bank account. As a business we don't give
+            stock tips, and have not authorized anyone to trade on behalf of
+            others. If you find anyone claiming to be part of EquiTrade and
+            offering such services, please create a ticket here.
+          </p>
+        </div>
+        <div className="text-center mt-4 pb-4" style={{ fontSize: "14px" }}>
+          <ul className="list-unstyled d-flex flex-wrap justify-content-center gap-4 m-0">
+            <li>
+              <a
+                rel="nofollow"
+                href="https://www.nseindia.com/"
+                style={{ textDecoration: "none", color: "gray" }}
+              >
+                NSE
+              </a>
+            </li>
+
+            <li>
+              <a
+                rel="nofollow"
+                href="https://www.bseindia.com/"
+                style={{ textDecoration: "none", color: "gray" }}
+              >
+                BSE
+              </a>
+            </li>
+
+            <li>
+              <a
+                rel="nofollow"
+                href="https://www.mcxindia.com/"
+                style={{ textDecoration: "none", color: "gray" }}
+              >
+                MCX
+              </a>
+            </li>
+
+            <li>
+              <a
+                rel="nofollow"
+                href="https://www.msei.in/"
+                style={{ textDecoration: "none", color: "gray" }}
+              >
+                MSEI
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="https://EquiTrade.com/terms-and-conditions/"
+                style={{ textDecoration: "none", color: "gray" }}
+              >
+                Terms & conditions
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="https://EquiTrade.com/policies-and-procedures/"
+                style={{ textDecoration: "none", color: "gray" }}
+              >
+                Policies & procedures
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="https://EquiTrade.com/privacy-policy/"
+                style={{ textDecoration: "none", color: "gray" }}
+              >
+                Privacy policy
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="https://EquiTrade.com/disclosure/"
+                style={{ textDecoration: "none", color: "gray" }}
+              >
+                Disclosure
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="https://EquiTrade.com/investor-attention/"
+                style={{ textDecoration: "none", color: "gray" }}
+              >
+                For investor's attention
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="https://EquiTrade.com/tos/investor-charter/"
+                style={{ textDecoration: "none", color: "gray" }}
+              >
+                Investor charter
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ frontend/src/landing_page/products/Universe.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+function Universe() {
+  return (
+    <div className="container mt-5">
+      <div className="row text-center">
+        <h1>The EquiTrade Universe</h1>
+        <p>
+          Extend your trading and investment experience even further with our
+          partner platforms
+        </p>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/Images/smallcaseLogo.png" style={{width:"200px",height:"50px"}}  />
+          <p className="text-small text-muted p-3">Themantic investment platform</p>
+        </div>
+         <div className="col-4 p-3 mt-5">
+          <img src="media/Images/streakLogo.png" style={{width:"200px",height:"50px"}}  />
+          <p className="text-small text-muted p-3">Algo & strategy platform</p>
+        </div>
+         <div className="col-4 p-3 mt-5">
+          <img src="media/Images/sensibullLogo.svg" style={{width:"200px",height:"50px"}}  />
+          <p className="text-small text-muted p-3">Options trading platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/Images/zerodhaFundhouse.png" style={{width:"200px",height:"50px"}}  />
+          <p className="text-small text-muted p-3">Asset management</p>
+        </div>
+         <div className="col-4 p-3 mt-5">
+          <img src="media/Images/goldenpiLogo.png" style={{width:"200px",height:"50px"}}  />
+          <p className="text-small text-muted p-3">Bonds trading platform</p>
+        </div>
+         <div className="col-4 p-3 mt-5">
+          <img src="media/Images/dittoLogo.png"style={{width:"200px",height:"50px"}}  />
+          <p className="text-small text-muted p-3">Insurance</p>
+        </div>
+        <button
+          className="p-2 btn btn-primary fs-5 mb-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Universe;
