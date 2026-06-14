497defcae863c5e464c59ef75e385ab5b0b6c1ca
Pruthwiraj Tripathy <pruthwirajtripathy2@gmail.com>
2026-06-14 05:35:53 +0530

    signup connection

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@ const isLocal = window.location.hostname === "localhost" || window.location.host
 
 export const DASHBOARD_URL = isLocal
   ? (window.location.port === "5173" ? "http://localhost:5174" : "http://localhost:5173")
-  : "https://equitrade-7eqx.onrender.com";
+  : "https://equitrade-dashboard-47xy.onrender.com";
 
 export const API_URL = isLocal
   ? "http://localhost:3002"
